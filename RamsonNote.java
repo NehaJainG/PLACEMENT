198aaf1f83b1b7b2c4de49a7a7d79c1b67e8fdae
NehaJainG <nehajaing1324@gmail.com>
2024-06-25 05:22:07 +0530

    RamsonNote problem

--- RamsonNote.java
+++ RamsonNote.java
@@ -36,13 +36,13 @@ public class RamsonNote {
         if(r.length() > m.length()) return false;
 
         HashMap<Character,Integer> map = new HashMap<Character,Integer>();
+        //add unique characters in m with it's frequency
         for(char c : m.toCharArray()){
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
 
         for(char c : r.toCharArray()){
             if(map.containsKey(c)){
-                //System.out.print(map.get(c));
                 map.put(c,map.get(c) - 1);
                 if(map.get(c) < 0){
                     return false;

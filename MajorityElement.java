bd93123f465447e4c053372a3fe1b086fb42b695
NehaJainG <nehajaing1324@gmail.com>
2024-06-26 19:53:23 +0530

    Majority element

--- MajorityElement.java
+++ MajorityElement.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.HashMap;
 
+
 public class MajorityElement {
     //using hash map to solve this
     public static int majorityElementHashMap(int[] nums) {
@@ -9,17 +10,20 @@ public class MajorityElement {
         for(int ele : nums){
             map.put(ele, map.getOrDefault(ele,0) + 1);
         }
+
         int n = nums.length / 2;
-        
         for(int key : map.keySet()){
             if( n < map.get(key)) return key;
-        }
-
+        } 
         return 0;
     }
-
+    
+    //this question assumes that majority element is always present
     //second approach is sorting the array and majority element will be present at n/2 position
     //because it occurs n/2 atleast
+    //unsorted: {2,2,1,1,1,2,2}
+    //sorted  : {2,2,2,2,1,1,1}
+    //n=7 ==> n/2 = 3 ==> (2)
 
     public static int majorityElementSorting(int[] nums){
         Arrays.sort(nums);

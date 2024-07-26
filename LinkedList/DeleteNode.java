afc88e86ce3131126ffb7b5118d06b42aa1ede93
NehaJainG <nehajaing1324@gmail.com>
2024-07-26 20:08:53 +0530

    changes

--- LinkedList/DeleteNode.java
+++ LinkedList/DeleteNode.java
@@ -15,4 +15,37 @@ public class DeleteNode {
 
         
     }
+
+    public ListNode deleteMiddle(ListNode head) {
+        if(head == null || head.next == null) return null;
+        ListNode fast = head; 
+        ListNode slow = head;
+
+        while(fast.next.next != null && fast.next.next.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return head;
+    }
+
+    public ListNode deleteMiddle2(ListNode head) {
+        //the same code but little improvision
+        if(head == null || head.next == null) return null;
+        ListNode fast = head; 
+        ListNode slow = head;
+
+        fast = fast.next.next;
+
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return head;
+    }
 }
